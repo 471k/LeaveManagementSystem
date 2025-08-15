e2850a2427aeaf2eb63c3c5d529040a5e2893121
alik <42393919+471k@users.noreply.github.com>
2025-08-15 15:54:43 +0200

    Seeding data in the database.

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagementSystem.Web.Data
@@ -8,6 +9,53 @@ namespace LeaveManagementSystem.Web.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                    { 
+                        Id = "6cf65df1-597c-447e-9e47-208ae83f4aea",
+                        Name = "Employee",
+                        NormalizedName = "EMPLOYEE"
+                    },
+                    new IdentityRole
+                    {
+                        Id = "7e336a5a-3f08-4d8c-986e-17e6d5233b90",
+                        Name = "Supervisor",
+                        NormalizedName = "SUPERVISOR"
+                    },
+                    new IdentityRole
+                    { 
+                        Id = "c2909634-c00e-4491-b6ac-1de88f7eeef1",
+                        Name = "Administrator",
+                        NormalizedName = "ADMINISTRATOR"
+                    }
+                );
+
+            var hasher = new PasswordHasher<IdentityUser>();
+            builder.Entity<IdentityUser>().HasData(
+                    new IdentityUser { 
+                        Id = "297d438f-7cf3-4f90-84a9-8598959e95bb",
+                        Email = "admin@localhost.com",
+                        NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                        NormalizedUserName = "ADMIN@LOCALHOST.COM",
+                        UserName = "admin@localhost.com",
+                        PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                        EmailConfirmed = true
+                    }
+                );
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                    new IdentityUserRole<string>
+                    { 
+                        RoleId = "c2909634-c00e-4491-b6ac-1de88f7eeef1",
+                        UserId = "297d438f-7cf3-4f90-84a9-8598959e95bb"
+                    }
+                );
         }
 
         public DbSet<LeaveType> LeaveTypes{ get; set; }
